f70a8988924ded38aecadb8af8eb334ffae76ab2
joshjoll <josh.joll@gmail.com>
2019-02-04 12:04:15 -0600

    event listner for overlay

---
+++ js/main.js
@@ -0,0 +1,180 @@
+//const
+
+//topics.*topic*.q*#*.*value*
+
+var topics = {
+  HTMLTags: {
+    v100: {
+      q1: {
+        a: 'A self closing tag',
+        q: 'what is <img>?',
+        value: '$100',
+      },
+      q2: {
+        a: 'A self closing tag',
+        q: 'what is <img>?',
+        value: '$200',
+      },
+      q3: {
+        a: 'A self closing tag',
+        q: 'what is <img>?',
+        value: '$300',
+      },
+      q4: {
+        a: 'A self closing tag',
+        q: 'what is <img>?',
+        value: '$400',
+      },
+      q5: {
+        a: 'A self closing tag',
+        q: 'what is <img>?',
+        value: '$500',
+      },
+    },
+    v200: {
+      q1: {
+        a: 'A self closing tag',
+        q: 'what is <img>?',
+        value: '$100',
+      },
+      q2: {
+        a: 'A self closing tag',
+        q: 'what is <img>?',
+        value: '$200',
+      },
+      q3: {
+        a: 'A self closing tag',
+        q: 'what is <img>?',
+        value: '$300',
+      },
+      q4: {
+        a: 'A self closing tag',
+        q: 'what is <img>?',
+        value: '$400',
+      },
+      q5: {
+        a: 'A self closing tag',
+        q: 'what is <img>?',
+        value: '$500',
+      },
+    },
+    v300: {
+      q1: {
+        a: 'A self closing tag',
+        q: 'what is <img>?',
+        value: '$100',
+      },
+      q2: {
+        a: 'A self closing tag',
+        q: 'what is <img>?',
+        value: '$200',
+      },
+      q3: {
+        a: 'A self closing tag',
+        q: 'what is <img>?',
+        value: '$300',
+      },
+      q4: {
+        a: 'A self closing tag',
+        q: 'what is <img>?',
+        value: '$400',
+      },
+      q5: {
+        a: 'A self closing tag',
+        q: 'what is <img>?',
+        value: '$500',
+      },
+    },
+    v400: {
+      q1: {
+        a: 'A self closing tag',
+        q: 'what is <img>?',
+        value: '$100',
+      },
+      q2: {
+        a: 'A self closing tag',
+        q: 'what is <img>?',
+        value: '$200',
+      },
+      q3: {
+        a: 'A self closing tag',
+        q: 'what is <img>?',
+        value: '$300',
+      },
+      q4: {
+        a: 'A self closing tag',
+        q: 'what is <img>?',
+        value: '$400',
+      },
+      q5: {
+        a: 'A self closing tag',
+        q: 'what is <img>?',
+        value: '$500',
+      },
+    },
+    v500: {
+      q1: {
+        a: 'A self closing tag',
+        q: 'what is <img>?',
+        value: '$100',
+      },
+      q2: {
+        a: 'A self closing tag',
+        q: 'what is <img>?',
+        value: '$200',
+      },
+      q3: {
+        a: 'A self closing tag',
+        q: 'what is <img>?',
+        value: '$300',
+      },
+      q4: {
+        a: 'A self closing tag',
+        q: 'what is <img>?',
+        value: '$400',
+      },
+      q5: {
+        a: 'A self closing tag',
+        q: 'what is <img>?',
+        value: '$500',
+      },
+    },
+
+  },
+  CSSProperties: {
+
+  },
+  arrays: {
+
+  },
+  typeof: {
+
+  },
+  gitHub: {
+
+  },
+  loops: {
+
+  },
+  jsFunctions: {
+
+  },
+  jquery: {
+
+  },
+};
+
+console.log(topics.HTMLTags.v100.q1.a);
+// app's state variables
+
+// cached element references
+
+// event listeners
+
+document.getElementById('play').addEventListener('click', removeOptions);
+
+// functions
+
+function removeOptions() {
+  document.querySelector('div').style.display = 'none';
+}
