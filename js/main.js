f24aa6de1e189cc47b113022d2312e69a24806b1
joshjoll <josh.joll@gmail.com>
2019-02-04 16:48:06 -0600

    fixed square's display

--- js/main.js
+++ js/main.js
@@ -164,7 +164,7 @@ var topics = {
   },
 };
 
-console.log(topics.HTMLTags);
+console.log(topics.HTMLTags.v100.q1);
 // app's state variables
 
 // cached element references
@@ -172,13 +172,22 @@ console.log(topics.HTMLTags);
 // event listeners
 
 document.getElementById('play').addEventListener('click', removeOptions);
-document.getElementsByClassName('cell')[0].addEventListener('click', revealAnswer);
+// document.querySelectorAll('.table-row')[0].addEventListener('click', revealAnswer); /*if row is clicked, deletes last .cell element */
+
+
+document.getElementsByClassName('board')[0].addEventListener('click', revealAnswer); /* hides all values when number clicked, but if row is clicked it deletes last .cell element*/
+
 
 // functions
 
 function revealAnswer(evt) {
-  console.log(evt.target);
-  evt.target.style.display = "none"
+  console.log(evt.target.className);
+  if (evt.target.className == 'answer' || evt.target.className == 'question' || evt.target.className == 'value') {
+    evt.target.style.display = 'none';
+    evt.target.nextElementSibling.style.display = 'inline';
+  } else {
+    console.log('Try another square');
+}
 }
 
 // function revealAnswer() {
